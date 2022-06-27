4ebdec1f686af4d923445fbbf0356352fa143380
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-27 14:42:27 +0300

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const {
+    Schema,
+    model,
+    Types: { ObjectId },
+} = require('mongoose');
+
+const userSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    coutry: { type: String, required: true },
+    avatar: { type: String, default: '/public/images/wvxPV9S.png' },
+    myMovies: [{ type: ObjectId, ref: 'Movie' }],
+});
+
+userSchema.index(
+    { email: 1 },
+    {
+        unique: true,
+        collation: {
+            locale: 'en',
+            strength: 2,
+        },
+    }
+);
+
+const User = model('User', userSchema);
