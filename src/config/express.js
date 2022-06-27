fcbbe5bfc80bb89931b42dbcfa358fc7efb8fb9a
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-27 16:55:52 +0300

    add auth middleware and validateToken

--- src/config/express.js
+++ src/config/express.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const auth = require('../middlewares/auth');
 
 module.exports = (app) => {
     app.use('/public', express.static('public'));
     app.use(express.json());
     app.use(cors());
+    app.use(auth());
 };
---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const { validateToken } = require('../services/user');
+
+module.exports = function () {
+    return (req, res, next) => {
+        const token = req.headers['x-authorization'];
+
+        if (token) {
+            try {
+                const payload = validateToken(token);
+
+                req.user = {
+                    email: payload.email,
+                    id: payload.id,
+                    token,
+                };
+            } catch (error) {
+                return res
+                    .status(401)
+                    .json({ message: 'Invalid access token. Please sign in.' });
+            }
+        }
+
+        next();
+    };
+};
