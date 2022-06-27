7e28689371c0b0f218ad2294ac6e310bdec51a5b
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-27 14:35:51 +0300

    add route for resource not found

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,5 +6,8 @@ router.get('/', (req, res) => {
     res.json({ message: 'AMoviesDB Service operational.' });
 });
 router.use('/api/catalog', movieController);
+router.all('*', (req, res) =>
+    res.status(400).json({ message: 'Resourse not found.' })
+);
 
 module.exports = router;
