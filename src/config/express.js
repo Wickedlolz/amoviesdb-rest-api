0ec560da28780da96cd45125a57b67ac8cf25535
Wickedlolz <iceshotgg.linux@gmail.com>
2022-07-01 18:48:12 +0300

    add cors origin

--- src/config/express.js
+++ src/config/express.js
@@ -5,6 +5,6 @@ const auth = require('../middlewares/auth');
 module.exports = (app) => {
     app.use('/public', express.static('public'));
     app.use(express.json());
-    app.use(cors());
+    app.use(cors({ origin: 'http://localhost:3000' }));
     app.use(auth());
 };
