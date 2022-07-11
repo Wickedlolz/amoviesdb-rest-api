532fdca7bcaceaafb7fa61cead8669b341313623
Wickedlolz <iceshotgg.linux@gmail.com>
2022-07-11 21:39:14 +0300

    add more origins

--- src/config/express.js
+++ src/config/express.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const auth = require('../middlewares/auth');
 
-const whiteList = ['http://localhost:3000'];
+const whitelist = ['http://localhost:3000', 'https://www.youtube.com'];
 
 module.exports = (app) => {
     app.use('/public', express.static('public'));
     app.use(express.json());
-    app.use(cors({ origin: 'http://localhost:3000' }));
+    app.use(cors({ origin: whitelist, credentials: true }));
     app.use(auth());
 };
