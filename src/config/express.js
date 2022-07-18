f686a8f73f03204e1a443132570e91b8607eb56a
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-07-18 13:03:19 +0300

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const auth = require('../middlewares/auth');
 
-const whitelist = ['https://amoviesdb.web.app/', 'https://www.youtube.com'];
+const whitelist = [
+    'http://localhost:3000',
+    'https://amoviesdb.web.app/',
+    'https://www.youtube.com',
+];
 
 module.exports = (app) => {
     app.use('/public', express.static('public'));
     app.use(express.json());
-    app.use(cors({ origin: 'https://amoviesdb.web.app/' }));
+    app.use(cors({ origin: whitelist, credentials: true }));
     app.use(auth());
 };
