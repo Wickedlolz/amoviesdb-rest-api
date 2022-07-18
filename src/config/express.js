ff3f8fb5349bcf235dd046cce622f2f4cf6d57de
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-07-18 12:57:18 +0300

    updating cors origin

--- src/config/express.js
+++ src/config/express.js
@@ -7,6 +7,6 @@ const whitelist = ['https://amoviesdb.web.app/', 'https://www.youtube.com'];
 module.exports = (app) => {
     app.use('/public', express.static('public'));
     app.use(express.json());
-    app.use(cors({ origin: whitelist, credentials: true }));
+    app.use(cors({ origin: 'https://amoviesdb.web.app/' }));
     app.use(auth());
 };
