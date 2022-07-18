d7595792e48844f1356edb2c6516d8fe118ad525
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-07-18 13:09:12 +0300

    remove whitelist for cors origin

--- src/config/express.js
+++ src/config/express.js
@@ -2,15 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const auth = require('../middlewares/auth');
 
-const whitelist = [
-    'http://localhost:3000',
-    'https://amoviesdb.web.app/',
-    'https://www.youtube.com',
-];
-
 module.exports = (app) => {
     app.use('/public', express.static('public'));
     app.use(express.json());
-    app.use(cors({ origin: whitelist, credentials: true }));
+    app.use(cors());
     app.use(auth());
 };
